0395c34935cf256564522145f759ea61c2eae4f5
QPTA <donhatquang401@gmail.com>
2026-06-15 14:23:04 +0700

    Add Exception 404,403 and 500

---
+++ src/main/java/com/darkfantasy/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.darkfantasy.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFound() {
+        return "error/404";
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public String forbidden() {
+        return "error/403";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String internalServerError(Exception ex) {
+
+        ex.printStackTrace();
+
+        return "error/500";
+    }
+}
